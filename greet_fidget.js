575381fb44183a78fb689d8791867b6fdad0e07f
FidelDhlamini <goodkidfidel@gmail.com>
2019-09-11 01:11:08 +0200

    initial commit

---
+++ greet_fidget.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.black(greet('Xola'));
+console.log(styledMessage)
+
+// using figlet
+const figlet = require('figlet');
+
+figlet('Xola', function(errorMsg, data) {
+    if (errorMsg) {
+        console.log('Something went wrong...');
+        console.dir(errorMsg);
+        return;
+    }
+    console.log(data)
+});
